c268dedf2ca8a791924e0149bb7c393564465d12
Piyush646 <55556638+Piyush646@users.noreply.github.com>
2021-01-13 22:31:00 +0530

    Add files via upload

---
+++ imgRotate.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+Mat rot(Mat photo, double angle)
+{
+    Mat rotate;
+    Point2f pt(src.cols/2., src.rows/2.);
+    Mat r = getRotationMatrix2D(pt, angle, 1.0);
+    warpAffine(src, dst, r, Size(src.cols, src.rows));
+    return rotate;
+}
+
+int main()
+{
+    Mat photo = imread("C:\Users\Akshat\Pictures\Screenshots\photo.png");
+
+    Mat rotate;
+    rotate = rot(photo, 30);
+
+    imshow("src", photo);
+    imshow("dst", rotate);
+    waitKey(0);
+    return 0;
+}
